8b28a8eed0056227d7d2ec921881e05cab0c889b
Shanu Kumar <shanu20122000@gmail.com>
2026-06-21 18:02:43 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A chrome plugin to to detect the sentiment of youtube video'scomment',
+    author='shanu kumar',
+    license='',
+)
